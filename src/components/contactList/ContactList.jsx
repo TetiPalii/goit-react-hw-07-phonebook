d5dcf0c102059b733544afc31b4c0299eafef46d
TetiPalii <TetianaPalii9491@gmail.com>
2023-07-25 22:24:52 +0200

    adding contacts

---
+++ src/components/contactList/ContactList.jsx
@@ -0,0 +1,48 @@
+import { useSelector, useDispatch } from 'react-redux';
+import css from './ContactList.module.css';
+
+import {
+  selectError,
+  selectFilter,
+  selectIsLoading,
+  selectItems,
+  selectVisibleContacts,
+} from 'redux/selectors';
+import { deleteContacts, fetchContacts } from 'redux/operations';
+import { useEffect } from 'react';
+
+export function ContactList() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const items = useSelector(selectItems);
+  const visibleContacts = useSelector(selectVisibleContacts);
+  // console.log(visibleContacts);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, []);
+
+  return (
+    <ul>
+      {isLoading && <p>Loading contacts...</p>}
+      {error && <p>{error.message}</p>}
+      {items.length > 0 &&
+        visibleContacts.map(({ id, name, number }) => (
+          <li key={id} className={css.item__contact}>
+            <p>
+              {name}: <span>{number}</span>
+            </p>
+            <button
+              className={css.contact__btn}
+              type="button"
+              onClick={() => {
+                dispatch(deleteContacts(id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+    </ul>
+  );
+}
